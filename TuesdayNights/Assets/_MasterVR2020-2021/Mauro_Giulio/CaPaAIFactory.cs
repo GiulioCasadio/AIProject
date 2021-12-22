f938a9adbfb8a7c595c859632ad6a6881a246249
ArdasAston <mauro.a.papagno@gmail.com>
2021-12-22 18:24:56 +0100

    Started CaPaAIFactory

---
+++ TuesdayNights/Assets/_MasterVR2020-2021/Mauro_Giulio/CaPaAIFactory.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TuesdayNights;
+
+namespace Ca_Pa
+{
+    public class CaPaAIFactory : tnBaseStandardMatchAIFactory
+    {
+        #region Roles
+
+        private static AIRole[] s_Roles_1 = new AIRole[]
+        {
+            AIRole.Midfielder
+        };
+        
+        private static AIRole[] s_Roles_2 = new AIRole[]
+        {
+            AIRole.Midfielder,
+            AIRole.Midfielder
+        };
+        
+        private static AIRole[] s_Roles_3 = new AIRole[]
+        {
+            AIRole.Midfielder,
+            AIRole.Midfielder,
+            AIRole.Midfielder
+        };
+
+        private static AIRole[][] s_Roles = new AIRole[][]
+        {
+            s_Roles_1,
+            s_Roles_2,
+            s_Roles_3
+        };
+        
+        #endregion
+        
+        private static AIRole s_DefaultRole = AIRole.Midfielder;
+        private List<AIRole> m_Roles = null;
+        protected int m_AICreated = 0;
+
+        #region Overrides
+
+        protected override void OnConfigure(tnTeamDescription i_TeamDescription)
+        {
+            if (i_TeamDescription == null)
+                return;
+
+            var charactersCount = i_TeamDescription.charactersCount;
+            
+            if (charactersCount <= 0 || charactersCount >= s_Roles.Length)
+                return;
+
+            var roles = s_Roles[charactersCount - 1];
+
+            if (roles == null || roles.Length == 0 || roles.Length != charactersCount)
+                return;
+
+            var aiIndex = 0;
+
+            for (var characterIndex = 0; characterIndex < charactersCount; ++characterIndex)
+            {
+                var characterDescription = i_TeamDescription.GetCharacterDescription(characterIndex);
+
+                if (characterDescription == null)
+                    continue;
+
+                var playerId = characterDescription.playerId;
+                var playerData = tnGameData.GetPlayerDataMain(playerId);
+
+                if (playerData != null) continue;
+                var role = roles[aiIndex++];
+                m_Roles.Add(role);
+            }
+
+            m_Roles.Sort();
+        }
+        
+        protected override tnStandardAIInputFillerBase OnCreateAI(int i_Index, GameObject i_Character)
+        {
+            if (m_Roles.Count == 0 || m_AICreated >= m_Roles.Count)
+            {
+                return CreateInputFiller(s_DefaultRole, i_Character);
+            }
+
+            var role = m_Roles[m_AICreated++];
+            return CreateInputFiller(role, i_Character);
+        }
+        
+        // INTERNALS
+
+        private static tnStandardAIInputFillerBase CreateInputFiller(AIRole i_Role, GameObject i_Character)
+        {
+            return new tnStandardAIInputFiller(i_Character, i_Role);
+        }
+
+        #endregion
+
+        public CaPaAIFactory()
+            : base()
+        {
+            m_Roles = new List<AIRole>();
+        }
+    }
+}
