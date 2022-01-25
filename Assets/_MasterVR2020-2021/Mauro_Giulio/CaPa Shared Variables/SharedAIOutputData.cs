6661e67664cecf0c793f22cbdc1bda06ea8c0c27
ArdasAston <mauro.a.papagno@gmail.com>
2022-01-25 20:26:59 +0100

    Added output variables

---
+++ Assets/_MasterVR2020-2021/Mauro_Giulio/CaPa Shared Variables/SharedAIOutputData.cs
@@ -0,0 +1,67 @@
+﻿using BehaviorDesigner.Runtime;
+using UnityEngine;
+
+namespace Ca_Pa.CaPa_SharedVariables
+{
+    [System.Serializable]
+    public class AIOutputData
+    {
+        public Vector2 axes;
+        public bool requestKick;
+        public bool requestDash;
+        public bool isAttracting;
+    }
+
+    [System.Serializable]
+    public class SharedAIOutputData : SharedVariable<AIOutputData>
+    {
+        public override string ToString()
+        {
+            return mValue == null ? "null" : mValue.ToString();
+        }
+
+        public static implicit operator SharedAIOutputData(AIOutputData value)
+        {
+            return new SharedAIOutputData { mValue = value };
+        }
+    }
+
+    public class AIOutputDataBuilder
+    {
+        private AIOutputData _data = new AIOutputData();
+
+        public static AIOutputDataBuilder Builder()
+        {
+            return new AIOutputDataBuilder();
+        }
+
+        public AIOutputDataBuilder WithAxes(Vector2 axes)
+        {
+            _data.axes = axes;
+            return this;
+        }
+
+        public AIOutputDataBuilder WithKick(bool kick)
+        {
+            _data.requestKick = kick;
+            return this;
+        }
+
+        public AIOutputDataBuilder WithDash(bool dash)
+        {
+            _data.requestDash = dash;
+            return this;    
+        }
+
+        public AIOutputDataBuilder WithAttract(bool attract)
+        {
+            _data.isAttracting = attract;
+            return this;
+        }
+
+        public AIOutputData Build()
+        {
+            return _data;
+        }
+    }
+}
