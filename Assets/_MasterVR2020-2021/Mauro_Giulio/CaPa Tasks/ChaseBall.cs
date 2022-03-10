22eb04a9c557b93fbf40fb009084226a320016d2
Giulio <giuliocasa@hotmail.com>
2022-03-10 19:21:47 +0100

    First conditional

---
+++ Assets/_MasterVR2020-2021/Mauro_Giulio/CaPa Tasks/ChaseBall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using Ca_Pa.CaPa_SharedVariables;
+
+public class ChaseBall : Action
+{
+    protected Task m_task;
+    private Transform ball;
+    protected Behavior m_owner => m_task.Owner;
+
+    public override void OnAwake()
+    {
+        //ball = AIInputData.ball;
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        /*if (Vector3.SqrMagnitude(transform.position - target.Value.position) < 0.1f)
+        {
+            return TaskStatus.Success;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target.Value.position, speed * Time.deltaTime);*/
+        return TaskStatus.Running;
+    }
+}
