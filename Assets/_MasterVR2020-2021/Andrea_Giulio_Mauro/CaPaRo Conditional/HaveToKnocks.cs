e902fa31a3b618952b6c44366f835c669267488c
Andrea <rosati.1595834@gmail.com>
2022-04-20 08:00:51 +0200

    Mi ero scordato di committare questi

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Conditional/HaveToKnocks.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Conditional/HaveToKnocks.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using BehaviorDesigner.Runtime.Tasks;
+using Ca_Pa_Ro.CaPaRo_SharedVariables;
 using UnityEngine;
 
 [TaskCategory("StatusCheck")]
 public class HaveToKnocks : Conditional
 {
     public PlayerFocus.SharedPlayerFocus focus;
-    
     public override TaskStatus OnUpdate()
     {
         if (focus.Value.m_state == PlayerFocus.PlayerStateFocus.KNOCKS)
