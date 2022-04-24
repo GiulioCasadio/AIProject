19b87f7c0e66b10d3686d8a44929ea80fcaa9f47
ArdasAston <mauro.a.papagno@gmail.com>
2022-04-24 17:39:20 +0200

    tackle opponent aggiunto ma da finire

---
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_TackleOpponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class A_TackleOpponent : A_Base
+{
+    public override TaskStatus OnUpdate()
+    {
+        var opponent = GetOpponentNearestTo(shared.Value.myPosition, shared.Value.m_Opponents);
+
+        output.Value.axes = opponent.position;
+
+        return TaskStatus.Success;
+    }
+}
