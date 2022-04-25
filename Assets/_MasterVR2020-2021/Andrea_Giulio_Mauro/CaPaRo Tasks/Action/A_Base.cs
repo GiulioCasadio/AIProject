05a176902685b2b4ef294d65c45c9e503aaf6b65
ArdasAston <mauro.a.papagno@gmail.com>
2022-04-25 17:22:52 +0200

    base changes and tackles changes

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_Base.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_Base.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
-
+using Ca_Pa_Ro;
 using Ca_Pa_Ro.CaPaRo_SharedVariables;
 
 public class A_Base : Action
@@ -23,6 +23,24 @@ public class A_Base : Action
     {
         shared = m_owner.GetVariable("Shared") as SharedAIInputData;
         output = m_owner.GetVariable("Output") as SharedAIOutputData;
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        ResetOutput(output);
+        return TaskStatus.Success;
+    }
+    
+    protected void ResetOutput(SharedAIOutputData tempOutput)
+    {
+        if (tempOutput == null)
+            return;
+
+        tempOutput.Value.axes = new Vector2(0, 0);
+
+        tempOutput.Value.requestKick = false;
+        
+        m_owner.SetVariableValue("Output", output);
     }
     #endregion
 
--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_TackleOpponent.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_TackleOpponent.cs
@@ -5,14 +5,17 @@ public class A_TackleOpponent : A_Base
 {
     public override TaskStatus OnUpdate()
     {
+        base.OnUpdate();
+        
         Transform opponent = GetOpponentNearestTo(shared.Value.myPosition, shared.Value.m_Opponents);
         Vector2 dir = ((shared.Value.myPosition - shared.Value.ballPosition) * -1).normalized;
 
-        if (Vector2.Distance(shared.Value.myPosition, shared.Value.ballPosition) < 1f)
+        //rendilo conditional
+        if (Vector2.Distance(shared.Value.myPosition, shared.Value.ballPosition) < 0.7f)
         {
             //reset axes
             output.Value.axes = new Vector2(0,0);
-            output.Value.requestTackle = true;
+            output.Value.requestKick = true;
         }
         else
         {
