23e82a195c9e84d7fcc27f5b5901e938e41e4b03
ArdasAston <mauro.a.papagno@gmail.com>
2022-04-25 16:40:02 +0200

    second changes to tackle

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_TackleOpponent.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_TackleOpponent.cs
@@ -5,9 +5,19 @@ public class A_TackleOpponent : A_Base
 {
     public override TaskStatus OnUpdate()
     {
-        var opponent = GetOpponentNearestTo(shared.Value.myPosition, shared.Value.m_Opponents);
+        Transform opponent = GetOpponentNearestTo(shared.Value.myPosition, shared.Value.m_Opponents);
+        Vector2 dir = ((shared.Value.myPosition - shared.Value.ballPosition) * -1).normalized;
 
-        output.Value.axes = opponent.position;
+        if (Vector2.Distance(shared.Value.myPosition, shared.Value.ballPosition) < 1f)
+        {
+            //reset axes
+            output.Value.axes = new Vector2(0,0);
+            output.Value.requestTackle = true;
+        }
+        else
+        {
+            output.Value.axes = dir;
+        }
 
         return TaskStatus.Success;
     }
