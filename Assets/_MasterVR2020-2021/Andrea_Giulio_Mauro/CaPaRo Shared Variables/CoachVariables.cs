3dc40f162f41f5042bba8179ecf12bbca35cc571
Andrea <rosati.1595834@gmail.com>
2022-04-29 00:49:10 +0200

    Coach da ordini

---
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Shared Variables/CoachVariables.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using UnityEngine;
+
+namespace Coach
+{
+    public class CoachVariables
+    {
+        public List<CoachPlayerCommunication> playersCommunications = new List<CoachPlayerCommunication>();
+        // Start is called before the first frame update
+
+        [System.Serializable]
+        public class SharedCoachVariables : SharedVariable<CoachVariables>
+        {
+            public override string ToString()
+            {
+                return mValue == null ? "null" : mValue.ToString();
+            }
+
+            public static implicit operator CoachVariables.SharedCoachVariables(CoachVariables value)
+            {
+                return new CoachVariables.SharedCoachVariables { mValue = value };
+            }
+
+        }
+    }
+}
