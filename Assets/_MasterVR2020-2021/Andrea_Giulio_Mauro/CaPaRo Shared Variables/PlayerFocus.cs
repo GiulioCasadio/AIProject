92032b678fc21b2d290dd1be53c2c882bae46bb7
Giulio <giuliocasa@hotmail.com>
2022-05-04 22:53:39 +0200

    Predisposizione copri area e controllo palla

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Shared Variables/PlayerFocus.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Shared Variables/PlayerFocus.cs
@@ -8,7 +8,7 @@ namespace Ca_Pa_Ro.Player
     [System.Serializable]
     public class PlayerFocus
     {
-        public enum PlayerStateFocus { KNOCKS, CHASEBALL, MARK, MAKEFREE, NONE }
+        public enum PlayerStateFocus { KNOCKS, CHASEBALL, MARK, MAKEFREE, COVERAREA, NONE }
 
         public PlayerStateFocus m_state = PlayerStateFocus.NONE;
         public bool m_hurry = false;
--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Conditional/HaveToCoverPosition.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Conditional/HaveToCoverPosition.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 using Ca_Pa_Ro.Player;
 
 [TaskCategory("StatusCheck")]
-public class HaveToKnocks : Conditional
+public class HaveToCoverPosition : Conditional
 {
     [SerializeField]
     private SharedPlayerFocus m_playerFocus;
     public override TaskStatus OnUpdate()
     {
-        if (m_playerFocus.Value.m_state == PlayerFocus.PlayerStateFocus.KNOCKS)
+        if (m_playerFocus.Value.m_state == PlayerFocus.PlayerStateFocus.COVERAREA)
             return TaskStatus.Success;
         return TaskStatus.Failure;
     }
