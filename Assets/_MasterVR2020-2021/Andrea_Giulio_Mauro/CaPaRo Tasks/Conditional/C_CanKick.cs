05fb063c89aa627b9a170489730d8ccb22c763a0
ArdasAston <mauro.a.papagno@gmail.com>
2022-05-06 09:39:46 +0200

    può calciare conditional

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Conditional/C_CanKick.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Conditional/C_CanKick.cs
@@ -3,10 +3,5 @@ using BehaviorDesigner.Runtime.Tasks;
 
 public class C_CanKick : C_Base
 {
-    public override TaskStatus OnUpdate()
-    {
-        base.OnUpdate();
-
-        if (IsBallInFeets(shared.Value.myPosition) && )
-    }
+    
 }
