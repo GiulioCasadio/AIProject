dcb4888965fd9b63ba05e1a738642c7ee2c80e87
GiulioCasadio <37106859+GiulioCasadio@users.noreply.github.com>
2022-05-15 00:19:00 +0200

    Update A_MarkOpponent.cs

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_MarkOpponent.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_MarkOpponent.cs
@@ -9,13 +9,9 @@ public class A_MarkOpponent : A_Base
 
         Vector2 midPoint = new Vector2((ballPosition.x + targetPosition.x) / 2, (ballPosition.y + targetPosition.y) / 2);
 
-        // Questo dovrebbe essere nel conditional
-        /* if (midPoint == myPosition)
-        {
-            return TaskStatus.Failure;
-        }*/
+        Vector2 midMidPoint = new Vector2((midPoint.x + targetPosition.x) / 2, (midPoint.y + targetPosition.y) / 2);
 
-        Vector2 targetDirection = ((myPosition - midPoint) * -1).normalized;
+        Vector2 targetDirection = ((myPosition - midMidPoint) * -1).normalized;
 
         //go to that position
         output.Value.axes = targetDirection;
