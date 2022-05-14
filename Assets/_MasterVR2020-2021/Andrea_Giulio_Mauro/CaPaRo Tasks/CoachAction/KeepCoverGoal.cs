cc8b30611de0522d2fcdb98fc2ef42aeb3e5e491
Andrea <rosati.1595834@gmail.com>
2022-05-14 20:38:59 +0200

    KeepCover WIP

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/CoachAction/KeepCoverGoal.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/CoachAction/KeepCoverGoal.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using Coach;
 using BehaviorDesigner.Runtime.Tasks;
+using Ca_Pa_Ro.Player;
 
 
 [TaskCategory("Coach")]
 public class KeepCoverGoal : CoachBaseAction
 {
-    
+    public override TaskStatus OnUpdate()
+    {
+        int playerCoveringGoal = 0;
+
+        foreach (CoachPlayerCommunication cpc  in m_sharedCoachVariables.Value.playersCommunications)
+        {
+            if (cpc.m_playerFocus.m_state == PlayerFocus.PlayerStateFocus.COVERGOAL)
+            {
+                cpc.m_focusGiven = true;
+                playerCoveringGoal++;
+            }
+        }
+
+        if(playerCoveringGoal > 1)
+            return TaskStatus.Success;
+
+        if (playerCoveringGoal == 1 && m_sharedCoachVariables.Value.m_behavior != CoachVariables.TeamBehavior.DEFENSIVE)
+            return TaskStatus.Success;
+
+        
+        
+        return TaskStatus.Success;
+    }
 }
