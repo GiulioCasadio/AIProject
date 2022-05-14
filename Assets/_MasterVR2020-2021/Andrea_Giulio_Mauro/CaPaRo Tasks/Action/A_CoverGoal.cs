8d2d2b50add666bc11a6245cf9c626c7c2fb7ce7
GiulioCasadio <37106859+GiulioCasadio@users.noreply.github.com>
2022-05-15 00:06:21 +0200

    Cover goal logic

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_CoverGoal.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/Action/A_CoverGoal.cs
@@ -1,28 +1,27 @@
-using UnityEngine;
+﻿using UnityEngine;
 using BehaviorDesigner.Runtime.Tasks;
 
-public class A_MarkOpponent : A_Base
+public class A_CoverGoal : A_Base
 {
+    public float timer = Time.deltaTime;
     public override TaskStatus OnUpdate()
     {
         base.OnUpdate();
 
         Vector2 midPoint = new Vector2((ballPosition.x + targetPosition.x) / 2, (ballPosition.y + targetPosition.y) / 2);
 
-        // Questo dovrebbe essere nel conditional
-        /* if (midPoint == myPosition)
-        {
-            return TaskStatus.Failure;
-        }*/
+        Vector2 midMidPoint = new Vector2((midPoint.x + targetPosition.x) / 2, (midPoint.y + targetPosition.y) / 2);
 
-        Vector2 targetDirection = ((myPosition - midPoint) * -1).normalized;
+        Vector2 targetDirection = ((myPosition - midMidPoint) * -1).normalized;
 
         //go to that position
         output.Value.axes = targetDirection;
 
-        CheckHurry(myPosition, midPoint);
+        CheckHurry(myPosition, targetPosition);
 
         m_owner.SetVariableValue("Output", output);
+
         return TaskStatus.Running;
     }
 }
+
