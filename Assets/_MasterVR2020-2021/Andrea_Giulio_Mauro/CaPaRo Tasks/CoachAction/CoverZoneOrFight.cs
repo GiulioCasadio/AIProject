d850790a35ff45b87c170ceccea17ae64e5311ab
Andrea <rosati.1595834@gmail.com>
2022-05-15 04:07:55 +0200

    Difesa V01 fatta

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/CoachAction/CoverZoneOrFight.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/CoachAction/CoverZoneOrFight.cs
@@ -11,16 +11,47 @@ public class CoverZoneOrFight : CoachBaseAction
 {
     public override TaskStatus OnUpdate()
     {
-        
-        
-        foreach (CoachPlayerCommunication cpc  in m_sharedCoachVariables.Value.playersCommunications)
+        List<CoachPlayerCommunication> freePlayers = GetFreePlayers();
+
+        if (freePlayers.Count == 0) 
+            return TaskStatus.Success;
+
+        switch (m_sharedCoachVariables.Value.m_behavior)
         {
+            case CoachVariables.TeamBehavior.NEUTRAL:
+                foreach (CoachPlayerCommunication cpc in freePlayers)
+                {
+                    cpc.m_playerFocus.m_state = PlayerFocus.PlayerStateFocus.COVERZONE;
+                    cpc.m_playerFocus.m_targetPosition = GetMostAdvancedOpponent().GetPositionXY();
+                    cpc.m_playerFocus.m_hurry = false;
+                }
+                break;
+            case CoachVariables.TeamBehavior.DEFENSIVE:
+                foreach (CoachPlayerCommunication cpc in freePlayers)
+                {
+                    if (IsOpponentNearBall(shared.Value.ballRadius * 2))
+                    {
+                        cpc.m_playerFocus.m_state = PlayerFocus.PlayerStateFocus.KNOCKS;
+                        cpc.m_playerFocus.m_targetOpponent = GetMostOpponentNearBall();
+                    }
 
-            if (cpc.m_focusGiven)
-            {
-                
-            }
-
+                    else
+                    {
+                        cpc.m_playerFocus.m_state = PlayerFocus.PlayerStateFocus.COVERZONE;
+                        cpc.m_playerFocus.m_targetPosition = GetMostAdvancedOpponent().GetPositionXY(); //mettiti nella zona dell'avversario
+                    }
+                    
+                    cpc.m_playerFocus.m_hurry = false;
+                }
+                break;
+            case CoachVariables.TeamBehavior.AGGRESSIVE:
+                foreach (CoachPlayerCommunication cpc in freePlayers)
+                {
+                    cpc.m_playerFocus.m_state = PlayerFocus.PlayerStateFocus.COVERZONE;
+                    cpc.m_playerFocus.m_hurry = false;
+                    cpc.m_playerFocus.m_targetPosition = new Vector2(shared.Value.myGoal.position.x * -0.75f ,0); // 3/4 campo avversaria
+                }
+                break;
         }
         
         return TaskStatus.Success;
