425b5a8e206297d4a83c2b90bada8d3f64489b37
ArdasAston <mauro.a.papagno@gmail.com>
2022-05-22 01:30:26 +0200

    neutral advance finished (test)

--- Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/CoachConditional/CanPassBall.cs
+++ Assets/_MasterVR2020-2021/Andrea_Giulio_Mauro/CaPaRo Tasks/CoachConditional/CanPassBall.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using BehaviorDesigner.Runtime.Tasks;
 using UnityEngine;
 
 public class CanPassBall : CoachBaseConditional
 {
-    
+    public override TaskStatus OnUpdate()
+    {
+        return TaskStatus.Success;
+    }
 }
